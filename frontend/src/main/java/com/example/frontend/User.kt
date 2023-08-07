45fee8ecf9688eb9e42ac4a4aabe63ec9a53dc47
leeos20k <leeos20k@naver.com>
2023-08-07 18:03:57 +0900

    회원가입버튼누르면db저장(front)

--- frontend/src/main/java/com/example/frontend/User.kt
+++ frontend/src/main/java/com/example/frontend/User.kt
@@ -3,11 +3,11 @@ package com.example.frontend
 import com.google.gson.annotations.SerializedName
 
 data class User(
-    @SerializedName("id")
-    val id:String,
-    @SerializedName("password")
-    val password:String,
-    @SerializedName("email")
-    val email:String,
-    @SerializedName("phone")
-    val phone: String )
+    @SerializedName("uemail")
+    val uemail:String,
+    @SerializedName("upassword")
+    val upassword:String,
+    @SerializedName("uname")
+    val uname:String,
+    @SerializedName("unickname")
+    val unickname: String )
