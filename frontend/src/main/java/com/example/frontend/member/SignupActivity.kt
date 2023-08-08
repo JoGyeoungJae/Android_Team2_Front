ca2b07cdd7725744be036e48552e76ee3c42c0ba
leeos20k <leeos20k@naver.com>
2023-08-08 16:37:42 +0900

    갤러리앱,사진앱으로 프로필사진 업로드하기

--- frontend/src/main/java/com/example/frontend/member/SignupActivity.kt
+++ frontend/src/main/java/com/example/frontend/member/SignupActivity.kt
@@ -1,8 +1,16 @@
 package com.example.frontend.member
 
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
+import com.example.frontend.R
 import com.example.frontend.service.ApiService
 import com.example.frontend.dto.User
 import com.example.frontend.databinding.ActivitySignupBinding
@@ -12,20 +20,121 @@ import retrofit2.Response
 import retrofit2.Retrofit
 
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
 
 class SignupActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignupBinding
+    lateinit var filePath: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+        //갤러리, 사진앱으로 프로필설정하는 부분
+        //갤러리 요청 런처
+        val requestGalleryLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult())
+        {
+            try {
+                //calRatio는 원본의 사진을 얼마나 줄일지 비율 값을 나타냄
+                val calRatio = calculateInSampleSize(
+                    it.data!!.data!!,
+                    resources.getDimensionPixelSize(R.dimen.imgSize),
+                    resources.getDimensionPixelSize(R.dimen.imgSize)
+                )
+
+                val option = BitmapFactory.Options()
+                option.inSampleSize = calRatio
+
+                //이미지 로딩
+                //사진을 바이트 단위로 읽었음. inputStream : 이미지의 바이트 단위의 결과값
+                var inputStream = contentResolver.openInputStream(it.data!!.data!!)
+
+                val bitmap = BitmapFactory.decodeStream(inputStream, null, option)
+                inputStream!!.close()
+                inputStream = null
+                bitmap?.let {
+                    binding.userImageView.setImageBitmap(bitmap)
+                } ?: let{
+                    Log.d("kkang", "bitmap null")
+                }
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+
+
+        binding.galleryButton.setOnClickListener {
+            //갤러리 앱
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            intent.type = "image/*"
+            requestGalleryLauncher.launch(intent)
+        }
+
+        //카메라 요청 런처
+        val requestCameraFileLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult())
+        {
+            //카메라 앱
+            val calRatio = calculateInSampleSize(
+                Uri.fromFile(File(filePath)),
+                resources.getDimensionPixelSize(R.dimen.imgSize),
+                resources.getDimensionPixelSize(R.dimen.imgSize)
+            )
+            val option = BitmapFactory.Options()
+            option.inSampleSize = calRatio
+            val bitmap = BitmapFactory.decodeFile(filePath, option)
+            bitmap?.let {
+                binding.userImageView.setImageBitmap(bitmap)
+            }
+        }
+
+
+        binding.cameraButton.setOnClickListener {
+            //카메라 앱
+            //파일 준비
+            val timeStamp: String =
+                SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+            val storageDir: File? =
+                getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+            val file = File.createTempFile(
+                "JPEG_${timeStamp}_",
+                ".jpg",
+                storageDir
+            )
+            filePath = file.absolutePath
+            val photoURI: Uri = FileProvider.getUriForFile(
+                this,
+                "com.example.frontend.fileprovider", file
+            )
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+            requestCameraFileLauncher.launch(intent)
+
+        }
+        //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
+
+
+
+
+
+        //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+        //스프링에 데이터보내는 부분
         binding.signupButton.setOnClickListener {
             signUp()
         }
     }
+
+
+    
+
+
 
     private fun signUp() {
         val uemail = binding.signupEmail.text.toString()
@@ -59,4 +168,48 @@ class SignupActivity : AppCompatActivity() {
             }
         })
     }
+
+
+    //사진사이즈 조절하는 함수
+    private fun calculateInSampleSize(fileUri: Uri, reqWidth: Int, reqHeight: Int): Int {
+        //비트맵 객체 그대로 사용하면, 사진 원본을 그대로 사용해서 메모리 부족 현상 생김.
+        // 그래서, 옵션이라는 속성을 사용.
+        val options = BitmapFactory.Options()
+        // 실제 비트맵 객체를 생성하는 것 아니고, 옵션 만 설정하겠다라는 의미.
+        options.inJustDecodeBounds = true
+        try {
+            // 실제 원본 사진의 물리 경로에 접근해서, 바이트로 읽음.
+            // 사진을 읽은 바이트 단위.
+            var inputStream = contentResolver.openInputStream(fileUri)
+
+            //inJustDecodeBounds 값을 true 로 설정한 상태에서 decodeXXX() 를 호출.
+            //로딩 하고자 하는 이미지의 각종 정보가 options 에 설정 된다.
+            BitmapFactory.decodeStream(inputStream, null, options)
+            // 읽었던 원본의 사진의 메모리 사용은 반납.
+            inputStream!!.close()
+            // 객체를 null 초기화,//
+            inputStream = null
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        //비율 계산........................
+        val (height: Int, width: Int) = options.run { outHeight to outWidth }
+        var inSampleSize = 1
+        //inSampleSize 비율 계산
+        //height ,width 원본의 가로 세로 크기.
+        // reqHeight, reqWidth 원하는 크기 사이즈,
+        // 이것보다 크면 원본의 사이즈를 반으로 줄이는 작업을 계속 진행.
+        if (height > reqHeight || width > reqWidth) {
+
+            val halfHeight: Int = height / 2
+            val halfWidth: Int = width / 2
+
+            while (halfHeight / inSampleSize >= reqHeight && halfWidth / inSampleSize >= reqWidth) {
+                inSampleSize *= 2
+            }
+        }
+        return inSampleSize
+    }
+
+
 }
