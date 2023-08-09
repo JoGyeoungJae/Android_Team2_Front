58bc2a56b58a659262547309041c9a0d8c5ea4f2
leeos20k <leeos20k@naver.com>
2023-08-09 18:43:08 +0900

    회원가입 완료

--- frontend/src/main/java/com/example/frontend/member/LoginActivity.kt
+++ frontend/src/main/java/com/example/frontend/member/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.frontend.member
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.example.frontend.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -12,6 +13,32 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        binding.loginButton.setOnClickListener {
+            val email = binding.loginEmail.text.toString()
+            val password = binding.loginPassword.text.toString()
+
+            // 로그인 정보 확인 로직
+            if (validateLogin(email, password)) {
+                // 로그인 성공
+                Toast.makeText(this,"로그인성공",Toast.LENGTH_SHORT).show()
+
+            } else {
+                // 로그인 실패
+                Toast.makeText(this,"로그인실패",Toast.LENGTH_SHORT).show()
+
+            }
+        }
+    }
+
+    private fun validateLogin(email: String, password: String): Boolean {
+        // TODO: MySQL 데이터베이스에서 입력한 email과 password를 확인하여 로그인 여부 판단
+        // 여기서는 가상의 예시로 고정된 값을 사용합니다.
+        val validEmail = "user@example.com"
+        val validPassword = "password123"
+
+        return email == validEmail && password == validPassword
     }
 
 }
